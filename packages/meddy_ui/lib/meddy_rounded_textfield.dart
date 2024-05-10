35755a3946b1263ea59ae998a9ed4b1226ac9139
Mark Edmunds <mark.edmunds@hybrid-designs.co.uk>
2024-05-10 20:08:29 +0100

    Initial commit :sparkles:

---
+++ packages/meddy_ui/lib/meddy_rounded_textfield.dart
@@ -0,0 +1,34 @@
+library meddy_ui;
+
+import 'package:flutter/material.dart';
+
+class RoundedTextField extends StatelessWidget {
+  const RoundedTextField({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      decoration: InputDecoration(
+        filled: true,
+        fillColor: Colors.white,
+        hintText: 'Enter text here',
+        contentPadding: const EdgeInsets.symmetric(
+          vertical: 12.0,
+          horizontal: 16.0,
+        ),
+        border: const OutlineInputBorder(
+          borderRadius: BorderRadius.all(Radius.circular(30.0)),
+          borderSide: BorderSide.none,
+        ),
+        enabledBorder: OutlineInputBorder(
+          borderRadius: const BorderRadius.all(Radius.circular(30.0)),
+          borderSide: BorderSide(color: Colors.grey.shade300, width: 1.0),
+        ),
+        focusedBorder: const OutlineInputBorder(
+          borderRadius: BorderRadius.all(Radius.circular(30.0)),
+          borderSide: BorderSide(color: Colors.blue, width: 2.0),
+        ),
+      ),
+    );
+  }
+}
